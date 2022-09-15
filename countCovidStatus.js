1a1ce3d9441154234e7b49877bbfe55bf4933cad
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-15 10:43:33 +0700

    exam

--- countCovidStatus.js
+++ countCovidStatus.js
@@ -1,7 +1,20 @@
 const { template } = require('@babel/core')
 
-//put your student id, name, and surname here
+//64130500076 วริษฐา จันทร์วันเพ็ญ
 function countCovidStatus(covidStatusArray) {
-  //code here
+  let a = covidStatusArray;
+  
+  if (a==null || a==undefined){
+    return undefined;
+  }else if(a!=='negative and low risk' || a!=='negative and high risk' || a!=='positive'){
+    return { };
+  }else if(a!=='negative and low risk' && a!=='negative and high risk' && a!=='positive'){
+    return { };
+  }else { 
+    const counts = {};
+    a.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+    return counts
+  }
 }
 module.exports = countCovidStatus
+
