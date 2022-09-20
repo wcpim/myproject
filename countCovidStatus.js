9766cc5f64103f7ca4e6cc0994308ee5313f5038
Gift78 <wimonsiri.2546@mail.kmutt.ac.th>
2022-09-20 11:57:45 +0700

    yo

--- countCovidStatus.js
+++ countCovidStatus.js
@@ -1,18 +1,57 @@
 const { template } = require('@babel/core')
 
 //64130500076 วริษฐา จันทร์วันเพ็ญ
-function countCovidStatus(covidStatusArray) {
-  let a = covidStatusArray;
-  
-  if (a==null || a==undefined){
-    return undefined;
-  }else if(a!=='negative and low risk' || a!=='negative and high risk' || a!=='positive'){
-    return { };
-  }else { 
-    const counts = {};
-    a.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
-    return counts
+function countCovidStatus(covidStatusArray){
+  //code here    
+  if(covidStatusArray===null || covidStatusArray===undefined)
+   return undefined    
+   if(covidStatusArray.length===0) return {}
+  let negLow=0, negHigh=0, pos=0    
+  for(const status of covidStatusArray){
+      switch(status){
+          case 'negative and low risk': 
+              negLow++                
+              break            
+              case 'negative and high risk':
+              negHigh++               
+               break            
+               case 'positive':
+              pos++               
+               break       
+               }
   }
-}
+  const covidStatus={}
+  if(pos>0)
+       covidStatus.positive=pos    
+       if(negHigh>0)
+      covidStatus['negative and high risk']=negHigh    
+      if(negLow>0)
+      covidStatus['negative and low risk']=negLow    
+      return covidStatus}
+console.log(
+  countCovidStatus([
+    'negative and low risk',
+    'positive',
+    'negative and low risk',
+    'negative and high risk',
+    'positive',
+    'negative and low risk'  ])
+)
+console.log(countCovidStatus(['positive', 'negative and low risk', 'positive']))
+console.log(
+  countCovidStatus([
+    'negative and low risk',
+    'positive',
+    'negative and low risk',
+    'abnormal',
+    'negative and high risk',
+    'positive',
+    ' normal'  ])
+)
+console.log(countCovidStatus(['covid']))
+console.log(countCovidStatus([]))
+console.log(countCovidStatus(null))
+console.log(countCovidStatus(undefined))
+
 module.exports = countCovidStatus
 
